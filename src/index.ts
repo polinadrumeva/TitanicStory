c878ae8c75de5c7b15531769a81181fdd5ed0fcf
Polina <polina9130@gmail.com>
2024-01-04 14:02:43 +0200

    add router functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-const home = document.getElementById("home");
+import { homePage } from "./views/home";
+
+type RouterMap = {
+    [key : string] : string;
+};
+
+const router : RouterMap = {
+    '/': homePage,
+};
+
+const rootDiv = document.getElementById("root");
+if(rootDiv) {
+    rootDiv.innerHTML = router[window.location.pathname];
+};
+
+const onNavigate = (pathname : string) => {
+    const {origin} = window.location;
+    window.history.pushState({}, pathname, `${origin}${pathname}`);
+    
+    if(rootDiv) {
+        rootDiv.innerHTML = router[pathname];
+    };
+}; 
+
+const home : HTMLElement | null = document.getElementById("home");
 const about = document.getElementById("about");
 const contact = document.getElementById("contact");
+
+home?.addEventListener("click", () => {
+    onNavigate('/');
+});
---
+++ src/views/home.ts
@@ -0,0 +1,2 @@
+export const homePage = 
+   `<div>Hello</div>`;
